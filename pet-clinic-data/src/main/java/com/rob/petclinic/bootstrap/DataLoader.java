f46c17d6cc5108db16bbf169cd48069a12b1aff7
robinson <mgbahacho@aol.com>
2019-12-30 19:01:58 +0100

    Added petTypes and associations to users in DataLoader. closes #34

--- pet-clinic-data/src/main/java/com/rob/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.rob.petclinic.bootstrap;
 
 import com.rob.petclinic.model.Owner;
+import com.rob.petclinic.model.Pet;
 import com.rob.petclinic.model.PetType;
 import com.rob.petclinic.model.Vet;
 import com.rob.petclinic.services.OwnerService;
@@ -11,13 +12,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
-    private Logger logger = LoggerFactory.getLogger(DataLoader.class);
+    private final Logger logger = LoggerFactory.getLogger(DataLoader.class);
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
@@ -30,16 +33,32 @@ public class DataLoader implements CommandLineRunner {
 
         PetType dog = new PetType();
         dog.setName("Dog");
-        PetType savedDogType = petTypeService.save(dog);
+        PetType savedDogPetType = petTypeService.save(dog);
+        logger.info("added new dog pet type");
 
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType savedCatType = petTypeService.save(cat);
+        logger.info("added new cat pet type");
 
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123467234");
+        logger.info("added owner:{}", owner1.getFirstName());
+
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
+        logger.info("Connected petType/name:'{}/{}' to user:{}", mikesPet.getPetType().getName(),mikesPet.getName(),owner1.getFirstName());
+
 
 
         ownerService.save(owner1);
@@ -47,6 +66,20 @@ public class DataLoader implements CommandLineRunner {
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("234389544");
+        logger.info("added owner:{}", owner2.getFirstName());
+
+
+        Pet fionasPet = new Pet();
+        fionasPet.setPetType(savedCatType);
+        fionasPet.setOwner(owner1);
+        fionasPet.setBirthDate(LocalDate.now());
+        fionasPet.setName("Simba");
+        owner2.getPets().add(fionasPet);
+        logger.info("Connected petType/name:'{}/{}' to user:{}", fionasPet.getPetType().getName(),fionasPet.getName(),owner2.getFirstName());
+
 
         ownerService.save(owner2);
         logger.info("loaded Owners...");
--- pet-clinic-data/src/main/java/com/rob/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,26 @@
 package com.rob.petclinic.services.map;
 
 import com.rob.petclinic.model.Owner;
+import com.rob.petclinic.model.Pet;
+import com.rob.petclinic.model.PetType;
 import com.rob.petclinic.services.OwnerService;
+import com.rob.petclinic.services.PetService;
+import com.rob.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Owner findByLastName(String lastName) {
         return null;
@@ -25,7 +38,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if(object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    PetType petType = pet.getPetType();
+                    if (petType != null) {
+                        if (petType.getId() == null){
+                            pet.setPetType(petTypeService.save(petType));
+                        }
+                    }else {
+                        throw new RuntimeException("Pet type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }
+        return null;
     }
 
     @Override
