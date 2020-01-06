41f9025a0464be7f1952f63780acc5a2a611c4f7
robinson <mgbahacho@aol.com>
2020-01-06 14:10:19 +0100

    Updated profiles for JPA Service, tested active profiles. closes #48

--- pet-clinic-data/src/main/java/com/rob/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/services/map/OwnerMapService.java
@@ -6,11 +6,13 @@ import com.rob.petclinic.model.PetType;
 import com.rob.petclinic.services.OwnerService;
 import com.rob.petclinic.services.PetService;
 import com.rob.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
--- pet-clinic-data/src/main/java/com/rob/petclinic/services/map/SpecialtyMapService.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/services/map/SpecialtyMapService.java
@@ -2,11 +2,13 @@ package com.rob.petclinic.services.map;
 
 import com.rob.petclinic.model.Speciality;
 import com.rob.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
     @Override
     public Set<Speciality> findAll() {
