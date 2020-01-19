b2ff8fc7553b81f4e0591ac4dbd9054b9ed40814
robinson <mgbahacho@aol.com>
2020-01-19 05:28:42 -0400

    Search by last name working. closes #63

--- pet-clinic-data/src/main/java/com/rob/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/model/PetType.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Builder
 @Entity
-@ToString
 @Table(name = "types")
 public class PetType extends BaseEntity{
     private String name;
--- pet-clinic-data/src/main/java/com/rob/petclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/rob/petclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -63,6 +63,6 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public List<Owner> findAllByLastNameLike(String lastName) {
-        return ownerRepository.findAllByLastNameLike(lastName);
+        return ownerRepository.findAllByLastNameLikeIgnoreCase(lastName);
     }
 }
