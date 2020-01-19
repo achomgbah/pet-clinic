498df2de66a90902526c6a8856a85b1abb730aa7
robinson <mgbahacho@aol.com>
2020-01-19 06:33:49 -0400

    Added PetControllerTest and updated PetType lombok builder class. #56

---
+++ pet-clinic-web/src/test/java/com/rob/petclinic/controllers/PetControllerTest.java
@@ -0,0 +1,64 @@
+package com.rob.petclinic.controllers;
+
+import com.rob.petclinic.model.Owner;
+import com.rob.petclinic.model.PetType;
+import com.rob.petclinic.services.OwnerService;
+import com.rob.petclinic.services.PetService;
+import com.rob.petclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
+class PetControllerTest {
+
+    @Mock
+    PetService petService;
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @InjectMocks
+    PetController petController;
+
+    MockMvc mockMvc;
+
+    Owner owner;
+    Set<PetType> petTypes;
+
+    @BeforeEach
+    void setUp() {
+        owner = Owner.builder().id(1L).build();
+
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).name("Dog").build());
+        petTypes.add(PetType.builder().id(2L).name("Cat").build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(petController)
+                .build();
+    }
+
+    @Test
+    void populatePetTypes() {
+    }
+
+    @Test
+    void findOwner() {
+    }
+
+    @Test
+    void initOwnerBinder() {
+    }
+}
