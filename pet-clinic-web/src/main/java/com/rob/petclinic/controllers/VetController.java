c93a5c8e9a73f32a6d85cd2bf1d7ba5d997ac87e
robinson <mgbahacho@aol.com>
2020-01-21 07:28:49 -0400

    Implemented API endpoint

--- pet-clinic-web/src/main/java/com/rob/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/rob/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.rob.petclinic.controllers;
 
+import com.rob.petclinic.model.Vet;
 import com.rob.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -18,5 +23,11 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJSON() {
+        return vetService.findAll();
     }
 }
